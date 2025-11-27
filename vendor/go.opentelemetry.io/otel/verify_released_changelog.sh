e95b33b5dd88b6d5ed7cca3bbd5ef2df2720137b
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-11-27 18:19:32 +0100

    None on "Add vendor"
    [ghstack-poisoned]

--- vendor/go.opentelemetry.io/otel/verify_released_changelog.sh
+++ vendor/go.opentelemetry.io/otel/verify_released_changelog.sh
@@ -25,17 +25,17 @@ PREVIOUS_LOCKED_FILE="$TEMP_DIR/previous_locked_section.md"
 CURRENT_LOCKED_FILE="$TEMP_DIR/current_locked_section.md"
 
 # Extract released sections from the previous version
-awk '/^<!-- Released section -->/ {flag=1} /^<!-- Released section ended -->/ {flag=0} flag' "$PREVIOUS_FILE" >"$PREVIOUS_LOCKED_FILE"
+awk '/^<!-- Released section -->/ {flag=1} /^<!-- Released section ended -->/ {flag=0} flag' "$PREVIOUS_FILE" > "$PREVIOUS_LOCKED_FILE"
 
 # Extract released sections from the current version
-awk '/^<!-- Released section -->/ {flag=1} /^<!-- Released section ended -->/ {flag=0} flag' "$CURRENT_FILE" >"$CURRENT_LOCKED_FILE"
+awk '/^<!-- Released section -->/ {flag=1} /^<!-- Released section ended -->/ {flag=0} flag' "$CURRENT_FILE" > "$CURRENT_LOCKED_FILE"
 
 # Compare the released sections
 if ! diff -q "$PREVIOUS_LOCKED_FILE" "$CURRENT_LOCKED_FILE"; then
-  echo "Error: The released sections of the changelog file have been modified."
-  diff "$PREVIOUS_LOCKED_FILE" "$CURRENT_LOCKED_FILE"
-  rm -rf "$TEMP_DIR"
-  false
+    echo "Error: The released sections of the changelog file have been modified."
+    diff "$PREVIOUS_LOCKED_FILE" "$CURRENT_LOCKED_FILE"
+    rm -rf "$TEMP_DIR"
+    false
 fi
 
 rm -rf "$TEMP_DIR"
